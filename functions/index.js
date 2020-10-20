3275ba55a8f5d069b217954425038568d715fce3
Mark <mrk.rosenberg@gmail.com>
2020-10-20 11:49:46 -0600

    adds authentication middleware

--- functions/index.js
+++ functions/index.js
@@ -37,11 +37,51 @@ app.get('/posts', (req, res) => {
         .catch(err => console.log('collection error: ', err))
 });
 
-app.post('/post', (req, res) => {
+// Authentication Middleware
+const FBAuth = (req, res, next) => {
+
+    let idToken;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('no token found')
+        return res.status(403).json({
+            error: 'Unauthorized'
+        })
+    };
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                        .where('userId', '==', req.user.uid)
+                        .limit(1)
+                        .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Token error: ', err);
+            return res.status(403).json(err);
+        });
+        
+};
+
+app.post('/post', FBAuth, (req, res) => {
+
+    if (req.body.body.trim() === '' ) {
+        return res.status(400).json({
+            body: 'Body must not be empty'
+        })
+    };
 
     const newPost = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
